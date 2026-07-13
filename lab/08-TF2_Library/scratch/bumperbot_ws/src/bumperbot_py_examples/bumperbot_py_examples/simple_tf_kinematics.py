59cbd8eec81f24be78bd4d89c8dbd162a4989939
Harshil Mehta <mehtaharshil58@gmail.com>
2026-07-13 16:48:44 +0530

    TF2 Library: TF2 Listener

--- lab/08-TF2_Library/scratch/bumperbot_ws/src/bumperbot_py_examples/bumperbot_py_examples/simple_tf_kinematics.py
+++ lab/08-TF2_Library/scratch/bumperbot_ws/src/bumperbot_py_examples/bumperbot_py_examples/simple_tf_kinematics.py
@@ -6,6 +6,7 @@ from tf2_ros import TransformBroadcaster, TransformException
 from tf2_ros.buffer import Buffer
 from tf2_ros.transform_listener import TransformListener
 from geometry_msgs.msg import TransformStamped
+from tf_transformations import quaternion_from_euler, quaternion_multiply, quaternion_inverse
 
 
 class SimpleTfKinematics(Node):
@@ -50,6 +51,10 @@ class SimpleTfKinematics(Node):
         # Service Server
         self.get_transform_srv_ = self.create_service(GetTransform, "get_transform", self.getTransformCallback)
 
+        # Quaternion
+        self.last_orientation_ = quaternion_from_euler(0, 0, 0)
+        self.orientation_increment_ = quaternion_from_euler(0, 0, 0.05)
+    
 
     def timerCallback(self):
         self.dynamic_transform_stamped_.header.stamp = self.get_clock().now().to_msg()
@@ -58,14 +63,41 @@ class SimpleTfKinematics(Node):
         self.dynamic_transform_stamped_.transform.translation.x = self.last_x_ + self.x_increment_
         self.dynamic_transform_stamped_.transform.translation.y = 0.0
         self.dynamic_transform_stamped_.transform.translation.z = 0.0
-        self.dynamic_transform_stamped_.transform.rotation.x = 0.0
-        self.dynamic_transform_stamped_.transform.rotation.y = 0.0
-        self.dynamic_transform_stamped_.transform.rotation.z = 0.0
-        self.dynamic_transform_stamped_.transform.rotation.w = 1.0
+        # self.dynamic_transform_stamped_.transform.rotation.x = 0.0
+        # self.dynamic_transform_stamped_.transform.rotation.y = 0.0
+        # self.dynamic_transform_stamped_.transform.rotation.z = 0.0
+        # self.dynamic_transform_stamped_.transform.rotation.w = 1.0
 
+        # Euler to Quaternion
+        q = quaternion_multiply(self.last_orientation_, self.orientation_increment_)
+        self.dynamic_transform_stamped_.transform.rotation.x = q[0]
+        self.dynamic_transform_stamped_.transform.rotation.y = q[1]
+        self.dynamic_transform_stamped_.transform.rotation.z = q[2]
+        self.dynamic_transform_stamped_.transform.rotation.w = q[3]
 
         self.dynamic_tf_broadcaster_.sendTransform(self.dynamic_transform_stamped_)
+        self.last_x_ = self.dynamic_transform_stamped_.transform.translation.x
+        self.last_orientation_ = q
+        self.rotations_counter_ += 1
+        if self.rotations_counter_ >= 100:
+            self.orientation_increment_ = quaternion_inverse(self.orientation_increment_)
+            self.rotations_counter_ = 0
+
+    
+    def getTransformCallback(self, req, res):
+        self.get_logger().info("Requested Transform between %s and %s" % (req.frame_id, req.child_frame_id))
+        requested_transform = TransformStamped()
+        try:
+            requested_transform = self.tf_buffer_.lookup_transform(req.frame_id, req.child_frame_id, rclpy.time.Time())
+        except TransformException as e:
+            self.get_logger().error("An error occurred while transforming %s and %s: %s" %
+                         (req.frame_id, req.child_frame_id, e))
+            res.success = False
+            return res
         
+        res.transform = requested_transform
+        res.success = True
+        return res
     
 
 def main():
